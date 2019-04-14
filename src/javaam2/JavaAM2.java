42cc079f0b5f866b1f4e4cd5868b1babd5033c65
Theethawat <theethawat.s@outlook.co.th>
2019-04-14 23:48:41 +0700

    Set In Main Class

--- src/javaam2/JavaAM2.java
+++ src/javaam2/JavaAM2.java
@@ -15,7 +15,7 @@ public class JavaAM2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new ImageViewer();
     }
     
 }
